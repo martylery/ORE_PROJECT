14ff4634cd07d747c67caeccf4fc0d3698fb63d9
emoxi123 <emil.docev2000@gmail.com>
2018-03-22 22:29:24 +0200

    basic routing

--- Ore_project/config/routes.rb
+++ Ore_project/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :users
+
+  get '/users/new', to: 'users#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
