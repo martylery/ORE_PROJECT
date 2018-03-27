cf0b20420e967b9c2d2e66dd2dc9a3a828436523
emoxi123 <emil.docev2000@gmail.com>
2018-03-27 11:39:24 +0300

    needed authentication for deleting posts (still not working)

--- Ore_project/config/routes.rb
+++ Ore_project/config/routes.rb
@@ -2,9 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   resources :posts
 
-  get '/posts/new', to: 'posts#new'
+  # get '/posts/new', to: 'posts#new'
 
+  # get 'users/sign_out', to: 'devise/sessions#destroy'
   root to: 'posts#index'
+
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
