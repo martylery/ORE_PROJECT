64ccbaeacc815ad7772c8f27e50fdfa6d43038e1
emoxi123 <emil.docev2000@gmail.com>
2018-03-27 11:26:24 +0300

    Sign up and sign in forms added

--- Ore_project/config/routes.rb
+++ Ore_project/config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   get '/posts/new', to: 'posts#new'
 
   root to: 'posts#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
