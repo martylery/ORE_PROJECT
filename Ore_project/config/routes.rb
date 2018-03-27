bbb2a7996c038cc68b7ea434a30ff07bb4eff9ec
emoxi123 <emil.docev2000@gmail.com>
2018-03-27 10:58:44 +0300

    db migration with sign in form

--- Ore_project/config/routes.rb
+++ Ore_project/config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :posts
-  resources :users
 
-  get '/users/new', to: 'users#new'
+  get '/posts/new', to: 'posts#new'
+
+  root to: 'posts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
