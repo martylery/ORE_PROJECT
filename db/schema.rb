6d76f1bd03fd64d713fecb297b71b967a6a4348e
martylery <jkata@mail.bg>
2018-05-26 15:17:08 +0300

    SyntaxError '.freeze; appears

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_26_110816) do
+ActiveRecord::Schema.define(version: 2018_05_26_115936) do
 
   create_table "artists", force: :cascade do |t|
     t.string "name"
     t.text "bio"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "type"
   end
 
   create_table "events", force: :cascade do |t|
